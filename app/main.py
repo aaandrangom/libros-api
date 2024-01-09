92ba67f1d124a927f85f9cd96df9fa882d6855e1
Anthony Andrango <96062307+aaandrangom@users.noreply.github.com>
2024-01-08 20:54:33 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(router_libros, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
